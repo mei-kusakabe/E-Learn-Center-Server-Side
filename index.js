9c4140eccf504f4d98108067e1d9afd7dfd7db79
Jafrin <jafrin3347@gmail.com>
2022-10-26 21:22:41 +0600

    fake-data-desc-added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
-// const news = require('./data/news.json');
+const news = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
@@ -16,16 +16,16 @@ app.get('/courseCategories', (req, res) => {
     res.send(categories)
 });
 
-// app.get('/courseCategories/:id', (req, res) => {
-//     const id = req.params.id;
-//     if (id === '08') {
-//         res.send(news);
-//     }
-//     else {
-//         const category_news = news.filter(n => n.category_id === id);
-//         res.send(category_news);
-//     }
-// })
+app.get('/courseCategories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '03') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
 
 // app.get('/news', (req, res) => {
 //     res.send(news);
