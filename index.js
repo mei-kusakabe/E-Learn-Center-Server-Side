6354502a91b7649c9b852baec0ead46be2be478e
Jafrin <jafrin3347@gmail.com>
2022-10-27 20:00:04 +0600

    server-second-last

--- index.js
+++ index.js
@@ -18,35 +18,19 @@ app.get('/courseCategories', (req, res) => {
 
 app.get('/courseCategories/:id', (req, res) => {
     const id = req.params.id;
-    // if (id === '03') {
-    //     res.send(news);
-    // }
-    // else {
     const category_news = news.filter(n => n.category_id === id);
     res.send(category_news);
-    // }
+
 })
 
 app.get('/checkout/:id', (req, res) => {
     const id = req.params.id;
-    // if (id === '03') {
-    //     res.send(news);
-    // }
-    // else {
+
     const category_news = news.filter(n => n.category_id === id);
     res.send(category_news);
-    // }
+
 })
 
-// app.get('/news', (req, res) => {
-//     res.send(news);
-// });
-
-// app.get('/news/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedNews = news.find(n => n._id === id);
-//     res.send(selectedNews);
-// });
 
 app.listen(port, () => {
     console.log('E-learn-centre Server running on port', port);
